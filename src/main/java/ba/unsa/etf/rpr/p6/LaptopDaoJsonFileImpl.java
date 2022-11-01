e3ca56834a07baf3c1198a64b617181fb4101064
Dino Kečo <dino.keco@gmail.com>
2022-11-01 11:49:38 +0100

    lecture 6 example fix

--- src/main/java/ba/unsa/etf/rpr/p6/LaptopDaoJsonFileImpl.java
+++ src/main/java/ba/unsa/etf/rpr/p6/LaptopDaoJsonFileImpl.java
@@ -1,25 +1,35 @@
 package ba.unsa.etf.rpr.p6;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LaptopDaoJsonFileImpl implements LaptopDao{
 
     private File file;
 
+    private List<Laptop> items = new ArrayList<Laptop>();
+
     public LaptopDaoJsonFileImpl(){
         this.file = new File("myObjects.json");
+        try {
+            this.items = (new ObjectMapper()).readValue(this.file, new TypeReference<List<Laptop>>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void save(Laptop laptop) throws IOException {
+        this.items.add(laptop);
         ObjectWriter ow = new ObjectMapper().writer();
-        String json = ow.writeValueAsString(laptop);
-
+        String json = ow.writeValueAsString(this.items);
         FileOutputStream fo = new FileOutputStream(this.file);
         fo.write(json.getBytes());
         fo.close();
@@ -27,7 +37,6 @@ public class LaptopDaoJsonFileImpl implements LaptopDao{
 
     @Override
     public Laptop get() throws IOException {
-        Laptop laptop = (new ObjectMapper()).readValue(new File("myObjects.json"), Laptop.class);
-        return laptop;
+        return this.items.get(0);
     }
 }
