fcb4e6efee3dd784adc75fc084a24721025529ba
Dino Kečo <dino.keco@gmail.com>
2022-11-28 15:46:08 +0100

    add category dao implementation. very dummy needs a lot of refactoring.

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,5 +1,10 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.dao.CategoryDao;
+import ba.unsa.etf.rpr.dao.CategoryDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Category;
+
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -11,12 +16,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        Stack<String> stack1 = new Stack<String>();
-        stack1.push("a");
-        stack1.push("b");
+        CategoryDao dao = new CategoryDaoSQLImpl();
 
-        System.out.println(stack1.pop());
-        System.out.println(stack1.toString());
-        System.out.println( "Dobro dosli u RPR" );
+
+        List<Category> categories = dao.getAll();
+        System.out.println(categories);
+
+        Category c2 = new Category();
+        c2.setId(2);
+        c2.setName("Courage");
+        dao.delete(2);
+        categories = dao.getAll();
+        System.out.println(categories);
     }
 }
