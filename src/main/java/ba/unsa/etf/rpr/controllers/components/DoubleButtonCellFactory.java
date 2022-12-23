419eb5fb581c648b0c82e124c3d18ac3f7ffcc92
Dino Kečo <dino.keco@gmail.com>
2022-12-23 19:07:53 +0100

    code cleanup

--- src/main/java/ba/unsa/etf/rpr/controllers/components/DoubleButtonCellFactory.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/components/DoubleButtonCellFactory.java
@@ -1,6 +1,5 @@
 package ba.unsa.etf.rpr.controllers.components;
 
-import ba.unsa.etf.rpr.domain.Quote;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.TableCell;
@@ -13,9 +12,9 @@ import javafx.util.Callback;
  */
 public class DoubleButtonCellFactory<T> implements Callback<TableColumn<T, T>, TableCell<T, T>> {
 
-    private EventHandler<ActionEvent> buttonOne;
+    private final EventHandler<ActionEvent> buttonOne;
 
-    private EventHandler<ActionEvent> buttonTwo;
+    private final EventHandler<ActionEvent> buttonTwo;
 
     /**
      *
@@ -26,12 +25,9 @@ public class DoubleButtonCellFactory<T> implements Callback<TableColumn<T, T>, T
         this.buttonOne = buttonOne;
         this.buttonTwo = buttonTwo;
     }
-    /**
-     * @param quoteObjectTableColumn
-     * @return
-     */
+
     @Override
     public TableCell<T, T> call(TableColumn<T, T> quoteObjectTableColumn) {
-        return new DoubleButtonTableCell<T>(buttonOne, buttonTwo);
+        return new DoubleButtonTableCell<>(buttonOne, buttonTwo);
     }
 }
