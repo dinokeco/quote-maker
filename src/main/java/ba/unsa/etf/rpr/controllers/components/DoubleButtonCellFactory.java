455b1f6de5203cea92550a3bbe0c435e3cc1e4a6
Dino Kečo <dino.keco@gmail.com>
2022-12-23 19:07:06 +0100

    javadoc for custom compoenents

--- src/main/java/ba/unsa/etf/rpr/controllers/components/DoubleButtonCellFactory.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/components/DoubleButtonCellFactory.java
@@ -7,12 +7,21 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.util.Callback;
 
+/**
+ * Double button cell factory for creation of buttons for each cell in the table
+ * @param <T>
+ */
 public class DoubleButtonCellFactory<T> implements Callback<TableColumn<T, T>, TableCell<T, T>> {
 
     private EventHandler<ActionEvent> buttonOne;
 
     private EventHandler<ActionEvent> buttonTwo;
 
+    /**
+     *
+     * @param buttonOne - event handler for first button (Edit)
+     * @param buttonTwo - event handler for second button (Delete)
+     */
     public DoubleButtonCellFactory(EventHandler<ActionEvent> buttonOne, EventHandler<ActionEvent> buttonTwo){
         this.buttonOne = buttonOne;
         this.buttonTwo = buttonTwo;
