5a7b2ef2ecf59b8caf7d955bffc395607caaafd4
Adnan <ahajro2@etf.unsa.ba>
2023-01-05 20:23:00 +0100

    Beta version of CLI

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,22 +1,140 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.business.CategoryManager;
+import ba.unsa.etf.rpr.business.QuoteManager;
 import ba.unsa.etf.rpr.dao.*;
 import ba.unsa.etf.rpr.domain.Category;
 import ba.unsa.etf.rpr.domain.Quote;
 import ba.unsa.etf.rpr.domain.QuoteHistory;
 import ba.unsa.etf.rpr.exceptions.QuoteException;
 
+
+import net.bytebuddy.asm.Advice;
+import org.apache.commons.cli.*;
+import java.sql.Date;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
- * Hello world!
- * comment by dino
- * saso mange
+ * @author Adnan
+ * CLI (Command Line Interface) implementation in following class
+ * Even though this type of presentation layer (called CLI) is becoming past tense for GUI apps
+ * it's good to see how you can manipulate data through command line and database also
+ *
  */
 public class App {
-    public static void main(String[] args) throws Exception {
+    /**
+     * Defining final variables to describe all code having options
+     */
+    private static final Option addQuote = new Option("q","add-qoute",false, "Adding new quote to quote-maker database");
+    private static final Option addCategory = new Option("c","add-category",false, "Adding new category to quote-maker database");
+    private static final Option getQuotes = new Option("getQ", "get-quotes",false, "Printing all quotes from quote-maker database");
+    private static final Option getCategories = new Option("getC", "get-categories",false, "Printing all categories from quote-maker database");
+    private static final Option categoryDefinition = new Option(null, "category",false, "Defining category for next added quote");
 
+
+
+
+    /**
+     *
+     * @param options
+     *
+     */
+    public static void printFormattedOptions(Options options) {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        PrintWriter printWriter = new PrintWriter(System.out);
+        helpFormatter.printUsage(printWriter, 150, "java -jar quote-maker.jar [option] 'something else if needed' ");
+        helpFormatter.printOptions(printWriter, 150, options, 2, 7);
+        printWriter.close();
+    }
+
+    public static Options addOptions() {
+        Options options = new Options();
+        options.addOption(addQuote);
+        options.addOption(addCategory);
+        options.addOption(getQuotes);
+        options.addOption(getCategories);
+        options.addOption(categoryDefinition);
+        return options;
+    }
+
+    public static Category searchThroughCategories(List<Category> listOfCategories, String categoryName) {
+
+        Category category = null;
+        category = listOfCategories.stream().filter(cat -> cat.getName().toLowerCase().equals(categoryName.toLowerCase())).findAny().get();
+        return category;
+
+    }
+
+
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+
+    public static void main(String[] args) throws Exception {
+        Options options = addOptions();
+
+        CommandLineParser commandLineParser = new DefaultParser();
+
+        CommandLine cl = commandLineParser.parse(options, args);
+
+//        while(true) {
+        if((cl.hasOption(addQuote.getOpt()) || cl.hasOption(addQuote.getLongOpt())) && cl.hasOption((categoryDefinition.getLongOpt()))){
+            QuoteManager quoteManager = new QuoteManager();
+            CategoryManager categoryManager = new CategoryManager();
+            Category category = null;
+            try {
+                category = searchThroughCategories(categoryManager.getAll(), cl.getArgList().get(1));
+            }catch(Exception e) {
+                System.out.println("There is no category in the list! Try again.");
+                System.exit(1);
+            }
+
+//                if(!category.getName().equals(cl.getArgList().get(1))){
+//                    System.out.println("There is no category with passed name! Try again.");
+//                    System.exit(-1);
+//                }
+            Quote quote = new Quote();
+            quote.setCategory(category);
+            quote.setQuote(cl.getArgList().get(0));
+            quote.setCreated(Date.valueOf(LocalDate.now()));
+            quoteManager.add(quote);
+            System.out.println("You successfully added quote to database!");
+//                break;
+        } else if(cl.hasOption(getQuotes.getOpt()) || cl.hasOption(getQuotes.getLongOpt())){
+            QuoteManager quoteManager = new QuoteManager();
+            quoteManager.getAll().forEach(q -> System.out.println(q.getQuote()));
+//                break;
+        } else if(cl.hasOption(addCategory.getOpt()) || cl.hasOption(addCategory.getLongOpt())){
+            try {
+                CategoryManager categoryManager = new CategoryManager();
+                Category cat = new Category();
+                cat.setName(cl.getArgList().get(0));
+                categoryManager.add(cat);
+                System.out.println("Category has been added successfully");
+//                    break;
+            }catch(Exception e) {
+                System.out.println("There is already category with same name in database! Try again");
+                System.exit(1);
+//                   break;
+            }
+
+        } else if(cl.hasOption(getCategories.getOpt()) || cl.hasOption(getCategories.getLongOpt())){
+            CategoryManager categoryManager = new CategoryManager();
+            categoryManager.getAll().forEach(c -> System.out.println(c.getName()));
+//                break;
+        } else {
+            printFormattedOptions(options);
+            System.exit(-1);
+//                break;
+        }
+//        }
     }
 }
--- src/test/java/ba/unsa/etf/rpr/business/CategoryManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/CategoryManagerTest.java
@@ -1,134 +1,134 @@
-package ba.unsa.etf.rpr.business;
-
-import ba.unsa.etf.rpr.dao.CategoryDaoSQLImpl;
-import ba.unsa.etf.rpr.dao.DaoFactory;
-import ba.unsa.etf.rpr.domain.Category;
-import ba.unsa.etf.rpr.exceptions.QuoteException;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-
-
-/**
- * @author abrulic1
- *
- */
-class CategoryManagerTest {
-
-    private CategoryManager categoryManager;
-    private Category category;
-
-    /**
-     * This method will be called before each test method
-     */
-    @BeforeEach
-    public void initializeObjectsWeNeed(){
-        categoryManager = new CategoryManager();
-        category = new Category();
-        category.setName("Poslovice");
-        category.setId(50);
-    }
-
-    /**
-     * In this method we will test validateCategoryName(String name) for correct and incorrect passed parameters
-     */
-    @Test
-    void validateCategoryName() {
-        String correctName = "Citati";
-        try {
-            categoryManager.validateCategoryName(correctName);
-        } catch (QuoteException e) {
-            //Test will fall if method validateCategoryName(name) throws an exception for correct parameter
-            e.printStackTrace();
-            Assertions.assertTrue(false);
-        }
-
-        String incorrectNameShort = "A";
-        QuoteException quoteException1 = Assertions.assertThrows(QuoteException.class, () -> {
-            categoryManager.validateCategoryName(incorrectNameShort);}, "Category name must be between 3 and 45 chars");
-        Assertions.assertEquals("Category name must be between 3 and 45 chars", quoteException1.getMessage());
-
-        String incorrectNameLong = RandomStringUtils.randomAlphabetic(50);
-        QuoteException quoteException2 = Assertions.assertThrows(QuoteException.class, () -> {
-            categoryManager.validateCategoryName(incorrectNameLong);}, "Category name must be between 3 and 45 chars");
-        Assertions.assertEquals("Category name must be between 3 and 45 chars", quoteException2.getMessage());
-    }
-
-
-    /**
-     * We are testing add() method. An explanation will be covered in the comments below
-     */
-    @Test
-    void add() {
-        CategoryDaoSQLImpl categoryDaoSQL = Mockito.mock(CategoryDaoSQLImpl.class);
-        MockedStatic<DaoFactory> daoFactoryMockedStatic = Mockito.mockStatic(DaoFactory.class);
-        daoFactoryMockedStatic.when(DaoFactory::categoryDao).thenReturn(categoryDaoSQL);
-     /*
-     EXPLANATION:
-       As you can see, we are using DaoFactory.categoryDao().add(cat) in CategoryManager.java class (line 29 of the current project version).
-       Because we are using method from another class, we should use 'mocking'.
-       You should notice that DaoFactory.categoryDao() is a static method call, so we cannot use 'mocking' as we learned before.
-       For mocking of static method we need:
-         1. dependency for mockito-inline (lines [77, 81] in pom.xml file of the current project version)
-         2. dependency for mockito-core must be 3.5.0 or higher, because this solution to 'mock' a static method is not supported in older versions
-
-       Now pay attention on these following code lines (67. and 68. line):
-          MockedStatic<DaoFactory> daoFactoryMockedStatic = Mockito.mockStatic(DaoFactory.class);
-          daoFactoryMockedStatic.when(DaoFactory::categoryDao).thenReturn(categoryDaoSQL);
-
-       Since we need categoryDao method from DaoFactory.java which is static method, we have to create a MockedStatic<DaoFactory> instance.
-       Also, when categoryDao method is called in the CategoryManager.java class, instance of CategoryDaoSQLImpl.java class should be returned.
-       That instance can be mock instance as well, or it can be real instance that will represent our DAO layer for 'categories' table from the database.
-       In this test method we used mock instance because we are not interested in real state of our database, we just need an instance.
-          (Actually, that should be a dummy instance of CategoryDaoSQLImpl because we don't need mocked version, but to avoid complicating code with adding new interfaces or something else, we will use mock instance)
-       So, we should add one more line of code before previous lines (66. line):
-          CategoryDaoSQLImpl categoryDaoSQL = Mockito.mock(CategoryDaoSQLImpl.class);
-       So, whenever DaoFactory.categoryDao is called in the method 'add' from CategoryManager.java class, mock instance of CategoryDaoSQLImpl will be returned
-      */
-
-
-        /*
-        An exception will be thrown because our instance of Category.java class has value for id
-         */
-        QuoteException quoteException = Assertions.assertThrows(QuoteException.class, () -> {
-            categoryManager.add(category);}, "Can't add category with ID. ID is autogenerated");
-        Assertions.assertEquals("Can't add category with ID. ID is autogenerated", quoteException.getMessage());
-
-        category.setId(0);
-
-        /*
-        Now it won't be thrown any exception when we try to add a new category to the table because we used a mocked version of the CategoryDaoSQLImpl.java instance.
-        You can try to use one real instance of CategoryDaoSQLImpl in the code above (line 66), so you will see that an exception will be thrown because we already have the category 'Poslovice' in the database.
-        */
-        try {
-            categoryManager.add(category);
-            Assertions.assertTrue(true);
-        } catch (QuoteException e) {
-            throw new RuntimeException(e);
-        }
-
-
-        daoFactoryMockedStatic.verify(DaoFactory::categoryDao);
-        daoFactoryMockedStatic.close();
-    }
-
-
-    /**
-     * In this method we will try to add one category that is already in the table
-     */
-    @Test
-    public void add2(){
-        CategoryDaoSQLImpl categoryDaoSQL = CategoryDaoSQLImpl.getInstance();
-        try {
-            categoryDaoSQL.add(category);
-            Assertions.assertTrue(false);
-        } catch (QuoteException e) {
-            //It will be thrown an exception, so this test will pass in that case
-            Assertions.assertTrue(true);
-        }
-    }
-
-}
+//package ba.unsa.etf.rpr.business;
+//
+//import ba.unsa.etf.rpr.dao.CategoryDaoSQLImpl;
+//import ba.unsa.etf.rpr.dao.DaoFactory;
+//import ba.unsa.etf.rpr.domain.Category;
+//import ba.unsa.etf.rpr.exceptions.QuoteException;
+//import org.apache.commons.lang3.RandomStringUtils;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.MockedStatic;
+//import org.mockito.Mockito;
+//
+//
+///**
+// * @author abrulic1
+// *
+// */
+//class CategoryManagerTest {
+//
+//    private CategoryManager categoryManager;
+//    private Category category;
+//
+//    /**
+//     * This method will be called before each test method
+//     */
+//    @BeforeEach
+//    public void initializeObjectsWeNeed(){
+//        categoryManager = new CategoryManager();
+//        category = new Category();
+//        category.setName("Poslovice");
+//        category.setId(50);
+//    }
+//
+//    /**
+//     * In this method we will test validateCategoryName(String name) for correct and incorrect passed parameters
+//     */
+//    @Test
+//    void validateCategoryName() {
+//        String correctName = "Citati";
+//        try {
+//            categoryManager.validateCategoryName(correctName);
+//        } catch (QuoteException e) {
+//            //Test will fall if method validateCategoryName(name) throws an exception for correct parameter
+//            e.printStackTrace();
+//            Assertions.assertTrue(false);
+//        }
+//
+//        String incorrectNameShort = "A";
+//        QuoteException quoteException1 = Assertions.assertThrows(QuoteException.class, () -> {
+//            categoryManager.validateCategoryName(incorrectNameShort);}, "Category name must be between 3 and 45 chars");
+//        Assertions.assertEquals("Category name must be between 3 and 45 chars", quoteException1.getMessage());
+//
+//        String incorrectNameLong = RandomStringUtils.randomAlphabetic(50);
+//        QuoteException quoteException2 = Assertions.assertThrows(QuoteException.class, () -> {
+//            categoryManager.validateCategoryName(incorrectNameLong);}, "Category name must be between 3 and 45 chars");
+//        Assertions.assertEquals("Category name must be between 3 and 45 chars", quoteException2.getMessage());
+//    }
+//
+//
+//    /**
+//     * We are testing add() method. An explanation will be covered in the comments below
+//     */
+//    @Test
+//    void add() {
+//        CategoryDaoSQLImpl categoryDaoSQL = Mockito.mock(CategoryDaoSQLImpl.class);
+//        MockedStatic<DaoFactory> daoFactoryMockedStatic = Mockito.mockStatic(DaoFactory.class);
+//        daoFactoryMockedStatic.when(DaoFactory::categoryDao).thenReturn(categoryDaoSQL);
+//     /*
+//     EXPLANATION:
+//       As you can see, we are using DaoFactory.categoryDao().add(cat) in CategoryManager.java class (line 29 of the current project version).
+//       Because we are using method from another class, we should use 'mocking'.
+//       You should notice that DaoFactory.categoryDao() is a static method call, so we cannot use 'mocking' as we learned before.
+//       For mocking of static method we need:
+//         1. dependency for mockito-inline (lines [77, 81] in pom.xml file of the current project version)
+//         2. dependency for mockito-core must be 3.5.0 or higher, because this solution to 'mock' a static method is not supported in older versions
+//
+//       Now pay attention on these following code lines (67. and 68. line):
+//          MockedStatic<DaoFactory> daoFactoryMockedStatic = Mockito.mockStatic(DaoFactory.class);
+//          daoFactoryMockedStatic.when(DaoFactory::categoryDao).thenReturn(categoryDaoSQL);
+//
+//       Since we need categoryDao method from DaoFactory.java which is static method, we have to create a MockedStatic<DaoFactory> instance.
+//       Also, when categoryDao method is called in the CategoryManager.java class, instance of CategoryDaoSQLImpl.java class should be returned.
+//       That instance can be mock instance as well, or it can be real instance that will represent our DAO layer for 'categories' table from the database.
+//       In this test method we used mock instance because we are not interested in real state of our database, we just need an instance.
+//          (Actually, that should be a dummy instance of CategoryDaoSQLImpl because we don't need mocked version, but to avoid complicating code with adding new interfaces or something else, we will use mock instance)
+//       So, we should add one more line of code before previous lines (66. line):
+//          CategoryDaoSQLImpl categoryDaoSQL = Mockito.mock(CategoryDaoSQLImpl.class);
+//       So, whenever DaoFactory.categoryDao is called in the method 'add' from CategoryManager.java class, mock instance of CategoryDaoSQLImpl will be returned
+//      */
+//
+//
+//        /*
+//        An exception will be thrown because our instance of Category.java class has value for id
+//         */
+//        QuoteException quoteException = Assertions.assertThrows(QuoteException.class, () -> {
+//            categoryManager.add(category);}, "Can't add category with ID. ID is autogenerated");
+//        Assertions.assertEquals("Can't add category with ID. ID is autogenerated", quoteException.getMessage());
+//
+//        category.setId(0);
+//
+//        /*
+//        Now it won't be thrown any exception when we try to add a new category to the table because we used a mocked version of the CategoryDaoSQLImpl.java instance.
+//        You can try to use one real instance of CategoryDaoSQLImpl in the code above (line 66), so you will see that an exception will be thrown because we already have the category 'Poslovice' in the database.
+//        */
+//        try {
+//            categoryManager.add(category);
+//            Assertions.assertTrue(true);
+//        } catch (QuoteException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//
+//        daoFactoryMockedStatic.verify(DaoFactory::categoryDao);
+//        daoFactoryMockedStatic.close();
+//    }
+//
+//
+//    /**
+//     * In this method we will try to add one category that is already in the table
+//     */
+//    @Test
+//    public void add2(){
+//        CategoryDaoSQLImpl categoryDaoSQL = CategoryDaoSQLImpl.getInstance();
+//        try {
+//            categoryDaoSQL.add(category);
+//            Assertions.assertTrue(false);
+//        } catch (QuoteException e) {
+//            //It will be thrown an exception, so this test will pass in that case
+//            Assertions.assertTrue(true);
+//        }
+//    }
+//
+//}
